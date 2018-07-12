1a14fe2bfdd717057aa314396a175cba22dc2b6a
Jiddo van Vliet <jiddo.van.vliet2@vaartland.nl>
2018-07-12 16:31:31 +0200

    default verzendkosten

--- app/code/community/Pimgento/Product/Model/Import.php
+++ app/code/community/Pimgento/Product/Model/Import.php
@@ -588,6 +588,12 @@ class Pimgento_Product_Model_Import extends Pimgento_Core_Model_Import_Abstract
 
         $values = array(
             'tax_class_id' => '_tax_class_id',
+            'weight'       => $this->_zde(
+                'IF(`verzendkosten` = "1", 1,
+                IF(`verzendkosten` = "0", 20,
+                IF(`verzendkosten` = "2", 100,
+                IF(`verzendkosten` = "3", 0, 0))))'
+            ),
         );
 
         $this->getRequest()->setValues($this->getCode(), 'catalog/product', $values, $this->getProductEntityTypeId(), 0, 2);
@@ -597,11 +603,6 @@ class Pimgento_Product_Model_Import extends Pimgento_Core_Model_Import_Abstract
             'enable_googlecheckout' => $this->_zde(0),
             'is_recurring'          => $this->_zde(0),
             'visibility'            => $this->_zde(4),
-            'weight'                => $this->_zde(
-                'IF(`verzendkosten` = "0", 20,
-                IF(`verzendkosten` = "1", 1,
-                IF(`verzendkosten` = "2", 100,
-                IF(`verzendkosten` = "3", 0, 20))))'),
         );
 
         if ($this->getConfig('configurable_enabled')) {
